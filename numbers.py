79476850fde572460fa267655d5545ab6e8efa52
5hyl0ck <32228692+5hyl0ck@users.noreply.github.com>
2020-07-07 22:32:50 -0400

    added some of the beginner solves

---
+++ numbers.py
@@ -0,0 +1,7 @@
+from Crypto.Util.number import long_to_bytes
+
+ciphertext = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+
+result = long_to_bytes(ciphertext, blocksize = 2)
+
+print(str(result))
