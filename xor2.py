063c3f48dda9dedd07c730f264f54cf29c527129
5hyl0ck <32228692+5hyl0ck@users.noreply.github.com>
2020-10-22 20:54:43 -0400

    Add files via upload

---
+++ xor2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3 
+from pwn import xor
+
+key1 = bytes.fromhex("a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313")
+secxor = bytes.fromhex("37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e")
+trdxor = bytes.fromhex("c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1")
+fthxor = bytes.fromhex("04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf")
+#if due to the commutative and self inverse laws, an xored string can be xored with one of the operands to
+#reveal the other
+key2 = xor(key1,secxor)
+key3 = xor(trdxor,key2)
+# running this on an already xored string
+buffer = xor(key1,trdxor)
+flag = xor(fthxor,buffer) 
+print(flag)
+
