cbc43777e3f038049bc7cf3b101e61e6785f3518
5hyl0ck <manny0ni14@gmail.com>
2020-10-22 23:42:11 -0400

    solved favourite bytegit add xor3.py git add xor3.py !

---
+++ xor3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3 
+
+from pwn import xor
+
+ciphertext = bytes.fromhex("73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d")
+
+for i in range(127):
+	flag = xor(bytes(chr(i), 'utf-8'),ciphertext)
+	flag = flag.decode("utf-8")
+
+	if "crypto" in flag:
+		print(flag)
