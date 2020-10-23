5f6a47afb2ca09780f4d6f2c0a013e9216384882
5hyl0ck <manny0ni14@gmail.com>
2020-10-22 23:56:46 -0400

    did something

--- xor3.py
+++ xor3.py
@@ -7,6 +7,5 @@ ciphertext = bytes.fromhex("73626960647f6b206821204f21254f7d694f7624662065622127
 for i in range(127):
 	flag = xor(bytes(chr(i), 'utf-8'),ciphertext)
 	flag = flag.decode("utf-8")
-
 	if "crypto" in flag:
 		print(flag)
