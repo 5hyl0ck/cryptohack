b8e1ee4fb9cea617b1994f0f4ca2ef36dded3644
5hyl0ck <manny0ni14@gmail.com>
2020-10-22 23:55:22 -0400

    removed pycache

--- numbers.py
+++ numbers.py
@@ -1,11 +1,9 @@
+#! /usr/bin/env/python3
+
 from Crypto.Util.number import long_to_bytes
 
 ciphertext = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
 
 result = long_to_bytes(ciphertext, blocksize = 2)
 
-<<<<<<< HEAD
 print(str(result))
-=======
-print(str(result))
->>>>>>> b0da0132c01df93654359eb2c956660ce27ab367
