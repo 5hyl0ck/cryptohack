a939e2e3c36cbc0ddffdb7f4a7ddf0062f30a0ab
5hyl0ck <manny0ni14@gmail.com>
2020-11-03 20:26:30 -0500

    updated

--- mod1.py
+++ mod1.py
@@ -1,10 +1,7 @@
-
-
+# a congruence a = b mod m implies that both a and b have the same value modulo m 
 
 one = 11 % 6
-
 two = 8146798528947 % 17
 
-
-
+# just fetch the minimum of the two modular calculations 
 print(min(one,two))
