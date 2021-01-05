f1ff35f18ee1ff9cc76aa18b35ea026cbdeab485
5hyl0ck <32228692+5hyl0ck@users.noreply.github.com>
2021-01-05 14:58:54 -0500

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
-=======
 
->>>>>>> b0da0132c01df93654359eb2c956660ce27ab367
 from pwn import * 
 import json
 import binascii
 import codecs
 
 r = remote('socket.cryptohack.org', 13377, level = 'debug')
-<<<<<<< HEAD
-		
-=======
-	
-	
->>>>>>> b0da0132c01df93654359eb2c956660ce27ab367
 def main():		
     received = json_recv()
     to_send = {"decoded": solveChallenge(received)}
     json_send(to_send)
     print(to_send)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b0da0132c01df93654359eb2c956660ce27ab367
 def json_recv():
     line = r.recvline()
     return json.loads(line.decode())
@@ -33,11 +20,6 @@ def json_send(stuff):
     request = json.dumps(stuff).encode()
     r.sendline(request)
     
-<<<<<<< HEAD
-=======
-
-
->>>>>>> b0da0132c01df93654359eb2c956660ce27ab367
 def solveChallenge(received):
     if received["type"] == "base64":
         decoded =  base64.b64decode(received["encoded"]).decode("utf-8")
@@ -53,12 +35,6 @@ def solveChallenge(received):
         for i in temp:
             decoded = decoded + chr(i)
     return decoded
-<<<<<<< HEAD
-                
-=======
-        
-        
->>>>>>> b0da0132c01df93654359eb2c956660ce27ab367
 i = 0        
 while i < 101:
     print("level " + str(i))
